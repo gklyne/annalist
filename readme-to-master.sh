ea7eadb32495d933e9608c9a212c0e683dbd780c
Graham Klyne <gk-github@ninebynine.org>
2014-01-31 13:54:05 +0000

    Updated installation instructions in README

---
+++ readme-to-master.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Transfer README.md file in develop branch to master branch
+
+MESSAGE=${1:-README updated from develop branch}
+
+if [ -f README.md ]; then
+
+    if git checkout master; then
+
+        git checkout develop README.md
+        git commit -m "$MESSAGE"
+        git checkout develop    
+
+    else
+
+        echo "git checkout problem (uncommitted files?) - aborting"
+
+
+    fi
+
+else
+
+    echo "no README.md file (wrong dirtectory?) - aborting"
+
+fi
+
+# End.
