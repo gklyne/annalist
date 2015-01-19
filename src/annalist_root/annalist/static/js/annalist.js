32e4f9a69375f7aa9f6ae771ff0a7b7a47b96dd4
Graham Klyne <gk-github@ninebynine.org>
2015-01-19 18:28:24 +0000

    When creating or copying entity, select content of entity_id ready to accept new value (uses JS)

--- src/annalist_root/annalist/static/js/annalist.js
+++ src/annalist_root/annalist/static/js/annalist.js
@@ -5,13 +5,15 @@
 annalist = {
 
     resize_handler: function (event) {
-        if (window.matchMedia(Foundation.media_queries['small']).matches){
+        if (window.matchMedia(Foundation.media_queries['small']).matches)
+        {
             $(".small-size-4").attr("size", 4);
             $(".small-rows-4").attr("rows", 4);
             /* $(".small-only-text-right").attr("text-align", "right"); doesn't work */
             $(".medium-add-margin").attr("width", "100%")
         };
-        if (window.matchMedia(Foundation.media_queries['medium']).matches){
+        if (window.matchMedia(Foundation.media_queries['medium']).matches)
+        {
             $(".medium-size-8").attr("size", 8);
             $(".medium-rows-8").attr("rows", 8);
             $(".medium-size-12").attr("size", 12);
@@ -22,3 +24,18 @@ annalist = {
 };
 
 $(window).resize(Foundation.utils.throttle(annalist.resize_handler, 10));
+
+$(document).ready( function ()
+{
+    /* For new of copy operations, select the entity_id field */
+    /* console.log("annalist.js ready") */
+    var e = $("input[type='hidden'][name='action']"); 
+    if (e.length) 
+    {
+        var newactions = ["new", "copy"]
+        if (newactions.indexOf(e.attr("value")) >= 0)
+        {
+            $("input[name='entity_id']").focus().select()
+        }
+    }
+});
