21aa47e2d40a864ac803474652c44f863b11f0c5
Graham Klyne <gk-github@ninebynine.org>
2019-10-15 14:56:15 +0100

    Update Django version (per security warning from GitHub)

--- src/annalist_root/runserver.sh
+++ src/annalist_root/runserver.sh
@@ -2,6 +2,6 @@
 # See also: gunicorn_server
 export DJANGO_SETTINGS_MODULE=annalist_site.settings.personal
 python manage.py migrate
-OAUTHLIB_INSECURE_TRANSPORT=1 annalist-manager runserver
+OAUTHLIB_INSECURE_TRANSPORT=1 annalist-manager runserver $1
 unset DJANGO_SETTINGS_MODULE
 
